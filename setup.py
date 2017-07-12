bd48777b674fe60c8c6c9d77500d771634b6e94a
Razvan Tudorica <razvantudorica@gmail.com>
2017-07-12 17:41:45 +0300

    add code url in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='tornados3',
       version='0.1',
       description='Tornado async manager for AWS S3',
-      #url='http://github.com/storborg/funniest',
+      url='https://github.com/raztud/tornados3',
       author='Razvan Tudorica',
       #author_email='',
       license='MIT',
