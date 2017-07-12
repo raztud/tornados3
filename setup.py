7d778002d1c296c80f6500a75b89e0791e22c2c5
Razvan Tudorica <razvantudorica@gmail.com>
2017-07-12 14:49:59 +0000

    add tornado in dependencies setup

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 
-setup(name='tornados3',
-      version='0.1',
-      description='Tornado async manager for AWS S3',
-      url='https://github.com/raztud/tornados3',
-      author='Razvan Tudorica',
-      #author_email='',
-      license='MIT',
-      packages=['tornados3'],
-      install_requires=[
-          'botocore',
-      ],
-      zip_safe=False)
-
+setup(
+    name='tornados3',
+    version='0.1',
+    description='Tornado async manager for AWS S3',
+    url='https://github.com/raztud/tornados3',
+    author='Razvan Tudorica',
+    license='MIT',
+    packages=['tornados3'],
+    install_requires=[
+        'botocore',
+        'tornado'
+    ],
+    zip_safe=False
+)
